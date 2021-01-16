044fa8abf11682d662c1d4bbcac808361977e0ff
Alicia Gorton <apersigehl@gmail.com>
2021-01-16 14:44:24 -0800

    adding new componet ListItem, with styling, and icons

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,23 +1,48 @@
 import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
+
 import Header from './components/Header';
-import 'react-native-get-random-values';
-import { v4 as uuidv4 } from 'uuid';
-uuidv4();
+import ListItem from './components/ListItem'
+
+import UUIDGenerator from 'react-native-uuid-generator';
+// import 'react-native-get-random-values';
+// import { v4 as uuidv4 } from 'uuid';
+import uuid from 'react-native-uuid';
+// uuid.v4();
 
 const App = () => {
 
   const [items, setItems] = useState([
-    {id: uuid(), text: 'apples'},
-    {id: uuid(), text: 'oranges'},
-    {id: uuid(), text: 'lemons'},
-    {id: uuid(), text: 'grapes'}
+    {
+      id: uuid(), 
+      text: 'apples',
+    },
+    {
+      id: uuid(), 
+      text: 'oranges',
+    },
+    {
+      id: uuid(), 
+      text: 'lemons',
+    },
+    {
+      id: uuid(), 
+      text: 'grapes',
+    },
   ]);
 
   return (
     // needs to use React Native Components
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem
+            item={item}
+          />
+        )}
+      />
     </View>
   );
 };
