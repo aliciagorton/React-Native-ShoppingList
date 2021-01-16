bc09d853d270744ba9089324e1f45b09269b6be4
Alicia Gorton <apersigehl@gmail.com>
2021-01-15 19:34:29 -0800

    adding styling and image component

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const App = () => {
   return (
     // need to use React Native Components
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+        style={styles.img}
+      />
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    // borderRadius: 100/2,
+  },
+});
 export default App;
