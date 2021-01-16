8388b69a759ce179e6549c83e152af17f6877ae2
Alicia Gorton <apersigehl@gmail.com>
2021-01-15 19:36:48 -0800

    deleting unnecessary code

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     // need to use React Native Components
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
-      />
     </View>
   );
 };
@@ -17,17 +13,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    // borderRadius: 100/2,
   },
 });
 export default App;
